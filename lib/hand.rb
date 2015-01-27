fe45ff74794de4f7438b7241679bb3b15df3146d
Andy Nicholson <andrew@anicholson.net>
2015-01-28 09:03:58 +1100

    Add easy hand comparison tests to Hand

--- lib/hand.rb
+++ lib/hand.rb
@@ -1,4 +1,24 @@
 require 'ostruct'
 
 class Hand < OpenStruct
+  RANKINGS_IN_DESCENDING_ORDER = [
+    :straight_flush,
+    :four_of_a_kind,
+    :full_house,
+    :flush,
+    :straight,
+    :three_of_a_kind,
+    :two_pair,
+    :pair,
+    :high_card
+  ]
+
+  def beats? other_hand
+    higher_ranking_hand_than?(other_hand)
+  end
+
+  private
+  def higher_ranking_hand_than?(other_hand)
+    RANKINGS_IN_DESCENDING_ORDER.index(hand) < RANKINGS_IN_DESCENDING_ORDER.index(other_hand.hand)
+  end
 end
--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -1,6 +1,27 @@
 require 'spec_helper'
 require 'hand'
 
-describe Hand do
+TYPES_OF_HAND = [:straight_flush, :four_of_a_kind, :full_house,
+:flush, :straight, :three_of_a_kind, :two_pair, :pair, :high_card ]
 
+describe Hand do
+  describe 'beats?' do
+    TYPES_OF_HAND.each do |type_of_hand|
+      context type_of_hand do
+        let(:this_hand) { Hand.new hand: type_of_hand }
+
+        TYPES_OF_HAND.each do |other_hand|
+          if TYPES_OF_HAND.index(other_hand) > TYPES_OF_HAND.index(type_of_hand)
+            it "beats #{other_hand}" do
+              expect(this_hand.beats?(Hand.new(hand: other_hand))).to eq(true)
+            end
+          elsif TYPES_OF_HAND.index(other_hand) < TYPES_OF_HAND.index(type_of_hand)
+            it "loses to #{other_hand}" do
+              expect(this_hand.beats?(Hand.new(hand: other_hand))).to eq(false)
+            end
+          end
+        end
+      end
+    end
+  end
 end
